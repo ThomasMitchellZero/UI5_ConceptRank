9d6ea3722af335711da9d87afaaa8282392335e4
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2019-08-10 15:30:35 -0400

    Back arrow functionality is working on the NotFound page.  Started adding the next route to the manifest.json file before I remembered to commit so not a total clean save.

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -4,7 +4,19 @@ sap.ui.define([
   "use strict";
 
   return BaseController.extend("TM.ConceptRank.controller.Home", {
+    onDisplayNotFound: function(oEvent){
+      //display the "notFound" target without changing the hash.  AFAICT the second argument (the object containing fromTarget) just adds its contents as properties of the display object.  
 
+			this.getRouter().getTargets().display("notFound", {
+        
+        // fromTarget is a key name we just made up.  It is not a hook, and could be called anything.
+        fromTarget: "targethome"
+      });
+    },
+
+    onNavToAddCollection: function(){
+      this.getRouter.navTo("")
+    }
   });
 
 });
--- webapp/controller/NotFound.controller.js
+++ webapp/controller/NotFound.controller.js
@@ -3,7 +3,35 @@ sap.ui.define([
 ], function (BaseController) {
   "use strict";
   return BaseController.extend("TM.ConceptRank.controller.NotFound", {
-     onInit: function () {
-     }
+    onInit: function () {
+
+
+      //  This section is a mystery.   Start the untangling here.
+      var oRouter, oTarget;
+			oRouter = this.getRouter();
+			oTarget = oRouter.getTarget("notFound");
+			oTarget.attachDisplay(function (oEvent) {
+        console.log(oTarget);
+        console.log(oEvent);
+        this._oData = oEvent.getParameter("data"); //store the data
+
+			}, this);
+    },
+
+    // override the parent's onNavBack (inherited from BaseController)
+		onNavBack : function (oEvent){
+
+      // If ._oData is not empty and ._oData.fromTarget is not empty
+			if (this._oData && this._oData.fromTarget) {
+
+        console.log(this._oData);
+        // ._oData.fromTarget is a key we specified, and its value is a link 
+				this.getRouter().getTargets().display(this._oData.fromTarget);
+				delete this._oData.fromTarget;
+				return;
+			}
+			// call the parent's onNavBack
+			BaseController.prototype.onNavBack.apply(this, arguments);
+		}
   });
 });
