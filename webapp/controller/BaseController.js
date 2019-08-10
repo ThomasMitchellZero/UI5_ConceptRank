a049165015e4b7981769a3bef4c50e5d551df14b
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2019-08-10 12:12:54 -0400

    Added in a few more descriptions

--- webapp/controller/BaseController.js
+++ webapp/controller/BaseController.js
@@ -1,12 +1,16 @@
 // One cool thing about this approach.  We're adding extra functionality to the controller component, but we only want these methods used, not the whole controller.  By naming it BaseController.js instead of BaseController.controller.js, we prevent this from ever being used in a view.
 
+//Because this has useful functionality that the base controller does not, all future controllers I make should be extended from this BaseController.js file, not the OG Controller component.
+
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/core/routing/History"
 ], function (Controller, History) {
 	"use strict";
 	return Controller.extend("TM.ConceptRank.controller.BaseController", {
-		getRouter : function () {
+    
+    // I suppose I should note that getRouter is a custom function, not an OTS function.  That might have been what was causing routing problems last time.
+    getRouter : function () {
 			return sap.ui.core.UIComponent.getRouterFor(this);
 		},
 		onNavBack: function (oEvent) {
